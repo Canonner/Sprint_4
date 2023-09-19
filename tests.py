50b4b3908fa7a9e23481ecec199c55ec6662fb06
Canonner <canonner@gmail.com>
2023-09-19 14:28:54 +0300

    Изменены и добавлены юнит-тесты в tests.py, уменьшено количество тестовых данных с 6 до 2 в фикстуре books_collector. Обновлен список тестов в README.md. В .gitignore добавлена папка .idea

--- tests.py
+++ tests.py
@@ -5,122 +5,110 @@ from main import BooksCollector
 
 class TestBooksCollector:
 
-    # Cписок для проверки метода add_new_book - новая книга, повтор новой книги
-    # и 4 книги для проверки классов "0", "от 1 до 40", "больше 40" символов - 0, 1, 39, 40 и 41 символ
-    books_to_add = [
-        'Смерть на ЗиЛе',
-        'Смерть на ЗиЛе',
-        '',
+    # 1. Тест для метода add_new_book - валидные книги - параметризация
+    # Используем валидные названия книг с длиной в 1, 2, 14, 39 и 40 символов
+    book_to_add = [
         'Ъ',
+        'ЪУ',
+        'Смерть на ЗиЛе',
         'Задачник по албанскому мату с решениями',
-        'Как править миром незаметно от санитаров',
-        '99 рецептов жарки мяса в ядерном реакторе'
+        'Как править миром незаметно от санитаров'
     ]
 
-    # 1. Тест для метода add_new_book
-    @pytest.mark.parametrize('name', books_to_add)
-    def test_add_new_book_seven_books_four_added(self, name):
+    @pytest.mark.parametrize('name', book_to_add)
+    def test_add_new_books_valid_book_one_added(self, name):
         collector = BooksCollector()
-        for i in self.books_to_add:
-            collector.add_new_book(i)
-        assert collector.get_books_genre() == {
-            'Смерть на ЗиЛе': '',
-            'Ъ': '',
-            'Задачник по албанскому мату с решениями': '',
-            'Как править миром незаметно от санитаров': ''
-        }
+        collector.add_new_book(name)
+        assert len(collector.get_books_genre()) == 1
 
-    # 2. Тест для метода set_book_genre - параметризация
+    # 2. Тест для метода add_new_book - две одинаковые книги c валидным названием
+    def test_add_new_books_two_same_books_one_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Смерть на ЗиЛе')
+        collector.add_new_book('Смерть на ЗиЛе')
+        assert len(collector.get_books_genre()) == 1
 
-    # Cписок для проверки метода - 6 книг с валидными названиями, у одной из которых нет жанра,
-    # одна книга с невалидным названем
-    # одна книга с валидным названием и невалидным жанром
-    eight_books_with_genre = [
-        ['Смерть на ЗиЛе', 'Фантастика'],
-        ['Как победить при Бородино в 6 палате', ''],
-        ['Операция ЪУЪ', 'Комедии'],
-        ['Задачник по албанскому мату с решениями', 'Мультфильмы'],
-        ['Как править миром незаметно от санитаров', 'Детективы'],
-        ['101 рецепт жарки мяса в ядерном реакторе', 'Ужасы'],
-        ['99 способов тушения жажды при помощи скипидара', 'Комедии'],
-        ['Кардинал Ришелье и три гвардейца', 'Любовные романы']
+    # 3. Тест для метода add_new_book - невалидные книги - параметризация
+    # Используем невалидные названия книг с длиной в 0, 41 и 46 символов
+    book_to_add = [
+        '',
+        '99 рецептов жарки мяса в ядерном реакторе',
+        'Самоучитель по игре на воображаемом контрабасе'
     ]
 
-    @pytest.mark.parametrize('name, genre', eight_books_with_genre)
-    def test_set_book_genre_eight_books_received_five_genres(self, name, genre):
+    @pytest.mark.parametrize('name', book_to_add)
+    def test_add_new_books_invalid_book_no_book_added(self, name):
         collector = BooksCollector()
-        for name in range(0, len(self.eight_books_with_genre)):
-            collector.add_new_book(self.eight_books_with_genre[name][0])
-            collector.set_book_genre(self.eight_books_with_genre[name][0], self.eight_books_with_genre[name][1])
-        assert list(collector.get_books_genre().values()) == [
-            'Фантастика',
-            '',
-            'Комедии',
-            'Мультфильмы',
-            'Детективы',
-            'Ужасы',
-            ''
-        ]
+        collector.add_new_book(name)
+        assert len(collector.get_books_genre()) == 0
 
-    # 3. Тест для метода get_book_genre - параметризация с фикстурой
-
-    # Cписок для проверки метода - 6 книг с валидными названиями, у одной из книг нет жанра
+    # 4. Тест для метода set_book_genre - параметризация
+    # Cписок для проверки метода - 2 книги с валидными названиями, у одной из которых нет жанра,
     books_with_genre = [
         ['Смерть на ЗиЛе', 'Фантастика'],
         ['Как победить при Бородино в 6 палате', ''],
-        ['Операция ЪУЪ', 'Комедии'],
-        ['Задачник по албанскому мату с решениями', 'Мультфильмы'],
-        ['Как править миром незаметно от санитаров', 'Детективы'],
-        ['101 рецепт жарки мяса в ядерном реакторе', 'Ужасы']
     ]
 
     @pytest.mark.parametrize('name, genre', books_with_genre)
-    def test_get_book_genre_six_books_five_genres(self, books_collector, name, genre):
-        assert books_collector.get_book_genre(name) == genre
+    def test_set_book_genre_two_books_received_one_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert list(collector.get_books_genre().values()) == [genre]
 
-    # 4. Тест для метода get_books_with_specific_genre - в запросе валидный жанр - с фикстурой
+    # 5. Тест для метода get_book_genre
+    def test_get_book_genre_one_book_received_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Операция ЪУЪ')
+        collector.set_book_genre('Операция ЪУЪ', 'Комедии')
+        assert collector.get_book_genre('Операция ЪУЪ') == 'Комедии'
+
+    # 6. Тест для метода get_books_with_specific_genre - в запросе валидный жанр - с фикстурой
     def test_get_books_with_specific_genre_valid_genre_book_received(self, books_collector):
-        assert books_collector.get_books_with_specific_genre('Комедии') == ['Операция ЪУЪ']
+        assert books_collector.get_books_with_specific_genre('Фантастика') == ['Смерть на ЗиЛе']
 
-    # 5. Тест для метода get_books_with_specific_genre - в запросе нет жанра - с фикстурой
+    # 7. Тест для метода get_books_with_specific_genre - в запросе нет жанра - с фикстурой
     def test_get_books_with_specific_genre_no_genre_no_book_received(self, books_collector):
         assert books_collector.get_books_with_specific_genre('') == []
 
-    # 6. Тест для метода get_books_genre - с фикстурой
-    def test_get_books_genre_six_books_received_dictionary(self, books_collector):
+    # 8. Тест для метода get_books_genre - с фикстурой
+    def test_get_books_genre_two_books_received_dictionary(self, books_collector):
         assert books_collector.get_books_genre() == {
             'Смерть на ЗиЛе': 'Фантастика',
-            'Как победить при Бородино в 6 палате': '',
-            'Операция ЪУЪ': 'Комедии',
-            'Задачник по албанскому мату с решениями': 'Мультфильмы',
             'Как править миром незаметно от санитаров': 'Детективы',
-            '101 рецепт жарки мяса в ядерном реакторе': 'Ужасы'
         }
 
-    # 7. Тест для метода get_books_for_children - с фикстурой
-    def test_get_books_for_children_six_books_received_three_books(self, books_collector):
+    # 9. Тест для метода get_books_for_children - с фикстурой
+    def test_get_books_for_children_two_books_received_one_book(self, books_collector):
         assert books_collector.get_books_for_children() == [
-            'Смерть на ЗиЛе',
-            'Операция ЪУЪ',
-            'Задачник по албанскому мату с решениями'
+            'Смерть на ЗиЛе'
         ]
 
-    # 8. Тест для метода add_book_in_favorites - параметризация с фикстурой
-    def test_add_book_in_favorites_three_books_received_one(self, books_collector):
+    # 10. Тест для метода add_book_in_favorites - с фикстурой
+    def test_add_book_in_favorites_one_book_received_one(self, books_collector):
         books_collector.add_book_in_favorites('Смерть на ЗиЛе')
-        # Повторяем ввод, чтобы проверить что одну и ту же книгу нельзя повторно добавить в Избранное
-        books_collector.add_book_in_favorites('Смерть на ЗиЛе')
-        # Проверяем, что новую книгу нельзя добавить в Избранное, если ее нет в списке книг
-        books_collector.add_book_in_favorites('Жизнь прекрасна')
         assert books_collector.get_list_of_favorites_books() == ['Смерть на ЗиЛе']
 
-    # 9. Тест для метода delete_book_from_favorites - с фикстурой
+    # 11. Тест для метода add_book_in_favorites - с фикстурой -
+    # Проверяем, что новую книгу нельзя добавить в Избранное, если ее нет в списке книг
+    def test_add_book_in_favorites_new_book_no_book_added(self, books_collector):
+        books_collector.add_book_in_favorites('Жизнь прекрасна')
+        assert books_collector.get_list_of_favorites_books() == []
+
+    # 12. Тест для метода add_book_in_favorites - с фикстурой
+    # Проверяем, что одну и ту же книгу нельзя повторно добавить в Избранное
+    def test_add_book_in_favorites_repeat_books_received_one(self, books_collector):
+        books_collector.add_book_in_favorites('Смерть на ЗиЛе')
+        books_collector.add_book_in_favorites('Смерть на ЗиЛе')
+        assert books_collector.get_list_of_favorites_books() == ['Смерть на ЗиЛе']
+
+    # 13. Тест для метода delete_book_from_favorites - с фикстурой
     def test_delete_book_from_favorites_one_book_deleted_empty_list(self, books_collector):
         books_collector.add_book_in_favorites('Смерть на ЗиЛе')
         books_collector.delete_book_from_favorites('Смерть на ЗиЛе')
         assert books_collector.get_list_of_favorites_books() == []
 
-    # 10. Тест для метода get_list_of_favorites_books - с фикстурой
+    # 14. Тест для метода get_list_of_favorites_books - с фикстурой
     def test_get_list_of_favorites_books_one_book_received_list_with_one_book(self, books_collector):
         books_collector.add_book_in_favorites('Смерть на ЗиЛе')
         assert books_collector.get_list_of_favorites_books() == ['Смерть на ЗиЛе']
